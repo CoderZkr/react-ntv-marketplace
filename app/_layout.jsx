24fb4e3e7038acf9f153ad46c1ba31ce76ca46a9
Kader ZKR <abdelkader.zekraoui1@gmail.com>
2024-05-26 15:49:11 +0100

    Updated auth screen

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,7 +1,9 @@
-import { StyleSheet, Text, View } from "react-native";
-import { SplashScreen, Stack } from "expo-router";
-import { useFonts } from "expo-font";
 import { useEffect } from "react";
+import { useFonts } from "expo-font";
+import "react-native-url-polyfill/auto";
+import { SplashScreen, Stack } from "expo-router";
+
+import GlobalProvider from "../context/GlobalProvider";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -18,6 +20,7 @@ const RootLayout = () => {
     "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
     "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
   });
+
   useEffect(() => {
     if (error) throw error;
 
@@ -33,10 +36,16 @@ const RootLayout = () => {
   if (!fontsLoaded && !error) {
     return null;
   }
+
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-    </Stack>
+    <GlobalProvider>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="search/[query]" options={{ headerShown: false }} />
+      </Stack>
+    </GlobalProvider>
   );
 };
 
